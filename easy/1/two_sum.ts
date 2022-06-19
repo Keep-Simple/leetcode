039e77d1fd0461b92373ef398567d2d5c0555ea3
Nikita Yasnogorodskyi <nikita@orca.security>
2022-06-19 23:33:50 +0300

    fix: synonyms test

--- easy/1/two_sum.ts
+++ easy/1/two_sum.ts
@@ -2,10 +2,7 @@ export function twoSum(nums: number[], target: number): number[] {
     for (let i = 0; i < nums.length; i++) {
         const num = nums[i]
 
-        for (let j = i + 1; j < nums.length; j++) {
-            if (num + nums[j] === target) {
-                return [i, j]
-            }
-        }
+        for (let j = i + 1; j < nums.length; j++)
+            if (num + nums[j] === target) return [i, j]
     }
 }
